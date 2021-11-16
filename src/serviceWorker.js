84c83f142b84233c101eb81ef08bbdd2d4bf1bab
swtbhsmn@gmail.com <swetabh.suman@datagrokr.com>
2021-11-16 08:14:37 +0530

    pwa-setup

---
+++ src/serviceWorker.js
@@ -0,0 +1,20 @@
+const serviceWorkerUrl = `${process.env.PUBLIC_URL}/sw.js`
+const serviceWorker = {
+  register:()=>{
+
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker.register(serviceWorkerUrl).then(function (registration) {
+          // Registration was successful
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+          // registration failed :(
+          console.log('ServiceWorker registration failed: ', err);
+        });
+      });
+    }
+  }
+    
+}
+
+export default serviceWorker;
